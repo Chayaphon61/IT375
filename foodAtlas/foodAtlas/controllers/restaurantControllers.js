50d4b751bd170d77cd033f8c140251443ec72132
Chayaphon <610108020001@dpu.ac.th>
2020-10-03 18:52:25 +0700

    FoodAtlas

---
+++ foodAtlas/foodAtlas/controllers/restaurantControllers.js
@@ -0,0 +1,21 @@
+const Restaurant = require('./../models/restaurantModel');
+
+exports.getAllRestaurants = (req,res) => {
+
+};
+
+exports.getRestaurant = (req, res) => {
+
+};
+
+exports.createRestaurant = (req,res) => {
+
+};
+
+exports.updateRestaurant = (req,res) => {
+
+};
+
+exports.deleteRestaurant = (req,res) => {
+    
+};
